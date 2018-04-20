81a1cf255ab9ab5e8535cddc6189f8ae3cd49f60
Ariel Juodziukynas <arieljuod@gmail.com>
2018-04-19 23:37:45 -0300

    Starting with "Options" form

--- app/controllers/setting_controller.rb
+++
@@ -1,8 +0,0 @@
-class SettingController < ApplicationController
-  def save
-    params[:key].each do |k,v|
-      Setting.set(k,v)
-    end
-    redirect_back fallback_location: root_path
-  end
-end
