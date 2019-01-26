85a22c3934098377f0c4e56352a143a8cf581bed
Ariel Juodziukynas <arieljuod@gmail.com>
2019-01-26 19:52:08 -0300

    Refactoring styles and HTML structure using CSS grids

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Dans
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    I18n.available_locales = [:es, :en]
+
+    I18n.default_locale = :es
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
