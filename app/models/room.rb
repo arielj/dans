0e3c3416bc0905d3c0d9163fc874441a3811ec09
Ariel Juodziukynas <arieljuod@gmail.com>
2019-01-30 12:24:28 -0300

    CSS fixes and validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   has_many :schedules
   has_many :klasses, through: :schedules
 
+  validates :name, uniqueness: true, presence: true
+
   def schedules_for_day(day)
     schedules.where(day: day).joins(:klass).where(Klass.arel_table[:status].eq(:active)).to_a
   end
