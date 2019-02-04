d26fd04d92dc0be7f47bdfda56886f93265419c3
Ariel Juodziukynas <arieljuod@gmail.com>
2019-02-04 00:20:01 -0300

    Functional language option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_admin!
+  before_action :set_locale
 
+  def set_locale
+    I18n.locale = Setting.fetch('language','es')
+  end
 end
