c5ed44428b78cdaa03d68d290b54b2b48b4bd8f2
Ariel Juodziukynas <arieljuod@gmail.com>
2019-12-22 00:06:05 -0300

    refactor js, fix debts table css

--- app/assets/javascripts/memberships.js
+++ app/assets/javascripts/memberships.js
@@ -1,4 +1,4 @@
-function refresh_amount(form) {
+function refreshAmount(form) {
   let schedulesIds = [...form.qsa('input.schedule:checked')].map( i => i.value);
   schedulesIds = schedulesIds.map( x => serializePair('schedules_ids[]', x)).join('&');
 
@@ -8,16 +8,16 @@ function refresh_amount(form) {
       const div = form.qs('.auto_calculation');
       let s = '';
       if (resp.fixedTotal !== "0,00")
-        s += 'Precio clases fijas: $' + resp.fixedTotal + "<br />";
+        s += `Precio clases fijas: $${resp.fixedTotal}<br />`;
 
       if (resp.durationTotal !== "0,00")
-        s += 'Precio por ' + resp.duration + 'hs: $' + resp.durationTotal + "<br />";
+        s += `Precio por ${resp.duration}hs: $${resp.durationTotal}<br />`;
 
-      s += 'Subtotal: $' + resp.subtotal + "<br />";
+      s += `Subtotal: $${resp.subtotal}<br />`;
       if (resp.discountTotal !== '0,00')
-        s += 'Descuento por grupo familiar (' + resp.discount + '): $' + resp.discountTotal + "<br />";
+        s += `Descuento por grupo familiar (${resp.discount}): $${resp.discountTotal}<br />`;
 
-      s += 'Total: $' + resp.total;
+      s += `Total: $${resp.total}`;
       div.innerHTML = s;
     }
   });
@@ -27,7 +27,7 @@ function bindNewMembership() {
   const form = byid('new_membership');
   const cal = form.qs('.schedules_calendar');
   Rails.delegate(cal, 'input.schedule', 'change', function(e) {
-    refresh_amount(this.form);
+    refreshAmount(this.form);
   });
   bindSchedulesCalendar();
 }
