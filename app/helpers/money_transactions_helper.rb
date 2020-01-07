558d14d15100332c0ef6c2a679bf51c638cfd04d
Ariel Juodziukynas <arieljuod@gmail.com>
2020-01-07 11:07:39 -0300

    pdf receipt styling

--- app/helpers/money_transactions_helper.rb
+++ app/helpers/money_transactions_helper.rb
@@ -16,28 +16,48 @@ module MoneyTransactionsHelper
   end
 
   def generate_pdf(items)
-    header_data = [[items.first.person.to_label, items.first.receipt.to_s, I18n.l(Date.today, format: :short)]]
+    header_data = [[
+      { content: items.first.person.to_label, width: 160 },
+      { content: "Nº#{items.first.receipt}", align: :center, width: 60 },
+      { content: I18n.l(Date.today, format: :receipt), align: :right, width: 160 }
+    ]]
 
-    table_header = %w[Fecha Descripción Monto]
-    table_content = items.map { |item| [I18n.l(item.created_at.to_date, format: :short), item.description.to_s, "$#{item.amount}"] }
-    table_footer = ['', '', "Total: #{items.map(&:amount).sum}"]
+    table_header = [
+      I18n.t(:date, scope: :receipts).upcase,
+      I18n.t(:description, scope: :receipts).upcase,
+      { content: I18n.t(:amount, scope: :receipts).upcase, align: :center }
+    ]
 
-    footer_data = [[Setting.fetch(:name, ''), '', '.............']]
+    table_content = items.map do |item|
+      [
+        { content: I18n.l(item.created_at.to_date, format: :receipt_item), width: 80 },
+        item.description.to_s,
+        { content: "$#{item.amount}", align: :right }
+      ]
+    end
+    table_footer = ['', { content: "Total: $#{items.map(&:amount).sum}", align: :right, colspan: 2 }]
+    table_data = [table_header] + table_content + [table_footer]
 
-    Prawn::Document.new(page_size: 'A6', page_layout: :landscape) do
-      table(header_data) do
+    footer_data = [[Setting.fetch(:name, ''), '', { content: '.......................', align: :center }]]
+
+    Prawn::Document.new(page_size: 'A6', page_layout: :landscape, margin: 15) do
+      table(header_data, width: 380, position: :center) do
         cells.borders = []
       end
 
-      move_down font.height * 2
+      move_down font.height * 1.5
 
-      table([table_header] + table_content + [table_footer]) do
-        cells.borders = []
+      table(table_data, width: 380, position: :center) do
+        cells.borders = [:bottom]
+        row(0).font_style = :bold
+        row(0).border_width = 2
+        row(-1).borders = []
+        row(-1).font_style = :bold
       end
 
-      move_down font.height * 2
+      move_cursor_to 30
 
-      table(footer_data) do
+      table(footer_data, width: 380, position: :center) do
         cells.borders = []
       end
     end.render
