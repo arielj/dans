ff199adbc18691fa898f656ec212d7ef4d0f1008
Ariel Juodziukynas <arieljuod@gmail.com>
2020-01-30 19:39:44 -0300

    add test to root page

---
+++ test/controllers/dashboard_controller_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class DashboardControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "shows a warning if there's no inscription for the current year" do
+    sign_in admins(:one)
+
+    get root_path
+    assert_response :success
+  end
+end
