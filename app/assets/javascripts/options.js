2df58dd1fb17f1dc9cc7bf7fd8eb365004ac011d
Ariel Juodziukynas <arieljuod@gmail.com>
2021-03-03 22:31:23 -0300

    Fix adding hourly prices for prices with discount

--- app/assets/javascripts/options.js
+++ app/assets/javascripts/options.js
@@ -1,30 +1,43 @@
 function bindAddPriceModal(form) {
   const hoursField = form.qs('input.hours');
-  const valueField = form.qs('input.value');
+  const valueWithoutDiscountField = form.qs('input.value_without_discount');
+  const valueWithDiscountField = form.qs('input.value_with_discount');
   const add = form.qs('button');
   add.addEventListener('click', function(e) {
     e.preventDefault();
     const hours = parseFloat(hoursField.value);
-    const fee = parseFloat(valueField.value);
+    const feeWithout = parseFloat(valueWithoutDiscountField.value);
+    const feeWith = parseFloat(valueWithDiscountField.value);
     if (hours < 1 || isNaN(hours)) {
       return alert('La cantidad de horas es inválida');
-    } else if (fee < 1 || isNaN(fee)) {
-      return alert('El precio es inválido');
+    } else if (feeWithout < 1 || isNaN(feeWithout)) {
+      return alert('El precio sin descuento es inválido');
+    } else if (feeWith < 1 || isNaN(feeWith)) {
+      return alert('El precio con descuento es inválido');
     } else if (byid('fee_' + hours)) {
       return alert('Ya hay un precio para esa cantidad de horas');
     } else {
       const row = document.createElement('TR');
+      row.dataset.hours = hours;
       const col1 = document.createElement('TD');
       const col2 = document.createElement('TD');
-      const input = document.createElement('INPUT');
-      input.type = 'number';
-      input.name = 'key[hour_fees][' + hours + ']';
-      input.id = 'fee_' + hours;
-      input.value = fee;
+      const col3 = document.createElement('TD');
+      const input1 = document.createElement('INPUT');
+      const input2 = document.createElement('INPUT');
+      input1.type = 'number';
+      input1.name = 'key[hour_fees][' + hours + '][]';
+      input1.id = 'fee_' + hours;
+      input1.value = feeWithout;
+      input2.type = 'number';
+      input2.name = 'key[hour_fees][' + hours + '][]';
+      input2.id = 'fee_' + hours;
+      input2.value = feeWith;
       col1.innerHTML = hours;
-      col2.appendChild(input);
+      col2.appendChild(input1);
+      col3.appendChild(input2);
       row.appendChild(col1);
       row.appendChild(col2);
+      row.appendChild(col3);
       const tbody = qs('.hourly_fees tbody');
       const trs = tbody.qsa('tr');
 
---
+++ test/system/settings_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "application_system_test_case"
+ 
+class SettingsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  driven_by :selenium, using: :headless_chrome
+
+  test 'adds fees per hours' do
+    sign_in admins(:operator)
+
+    Setting.set_hours_fees(0.5, [50, 45])
+
+    visit settings_path
+
+    click_on 'Agregar precio'
+
+    within '#add_price_form' do
+      fill_in 'Horas', with: '1'
+      fill_in 'Valor (sin descuento)', with: '100'
+      fill_in 'Valor (con descuento)', with: '90'
+      click_button 'Agregar'
+    end
+
+    tr = find('tr[data-hours="1"]')
+
+    assert_equal '1', tr.find('td:nth-child(1)').text
+    assert_equal '100', tr.find('td:nth-child(2) input').value
+    assert_equal '90', tr.find('td:nth-child(3) input').value
+
+    click_button 'Guardar opciones'
+
+    assert_text 'Configuración guardada'
+
+    assert_equal '50', Setting.get_hours_fee(0.5, with_discount: false)
+    assert_equal '45', Setting.get_hours_fee(0.5, with_discount: true)
+    assert_equal '100', Setting.get_hours_fee(1, with_discount: false)
+    assert_equal '90', Setting.get_hours_fee(1, with_discount: true)
+  end
+end
