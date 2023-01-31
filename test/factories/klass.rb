71513efa4e271ead355b35474eeb9dcb5e825916
Ariel Juodziukynas <arieljuod@gmail.com>
2023-01-31 19:20:45 -0300

    Fix tests

--- test/factories/klass.rb
+++ test/factories/klass.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :klass do
     sequence(:name) { |n| "Klass #{n}" }
     fixed_fee_cents { '50000' }
+    fixed_fee_with_discount_cents { '40000' }
 
     factory :klass_with_schedules do
       after(:build) do |k|
